3ea5d649c12afdd0bd95e4627d316914009c3bd0
Tyler Drews <tdrews@bamboohr.com>
2023-08-29 15:25:41 -0500

    Removing old schema types/resolvers

--- src/resolvers.js
+++ src/resolvers.js
@@ -57,6 +57,8 @@ const resolvers = {
 
 				if (measures.length && dimensions.length) {
 					data = groupList(data, dimensions);
+				} else {
+					data = [{ list: data }];
 				}
 			});
 
@@ -64,74 +66,10 @@ const resolvers = {
 				employees: employee,
 			}));
 		},
-	  employees: (parent, args) => {
-			const { groupBy } = args?.input || {};
-			let employees = companyData.employees;
-			if (groupBy) {
-				return groupList(employees, groupBy);
-			}
-
-			return [{ list: employees }];
-		},
-		turnover: (parent, args) => {
-			const { groupBy } = args?.input || {};
-			let turnover = companyData.turnover;
-			if (groupBy) {
-				return groupList(turnover, groupBy);
-			}
-
-			return [{ list: turnover }];
-		},
 	},
 	EmployeesMembers: {
 		count: (parent) => {
 			return parent.list.length;
-		},
-	},
-	GroupedList: {
-		total: (parent) => parent.list.length,
-		avg: (parent, args) => {
-			const list = parent?.list;
-			const { field } = args.input;
-			const sum = list.reduce((acc, listItem) => {
-				if (typeof listItem[field] !== 'number') {
-					throw new Error(`Field ${field} is not a number`);
-				}
-				return acc + listItem[field];
-			}, 0);
-			return sum / list.length;
-		},
-		sum: (parent, args) => {
-			const list = parent?.list;
-			const { field } = args.input;
-			return list.reduce((acc, listItem) => {
-				if (typeof listItem[field] !== 'number') {
-					throw new Error(`Field ${field} is not a number`);
-				}
-				return acc + listItem[field];
-			}, 0);
-		},
-		list: (parent, args) => {
-			const { groupBy } = args?.input || {};
-			let list = parent.list;
-			if (groupBy) {
-				return groupList(list, groupBy);
-			}
-			return list;
-		},
-	},
-	Iterable: {
-		__resolveType: (parent, args) => {
-			// TODO: Account for other iterable types
-			if (parent?.group) {
-				return 'GroupedList'; 
-			}
-
-			if (parent?.id) {
-				return 'Employee';
-			}
-			
-			return 'Turnover';
 		},
 	},
 }
--- src/typeDefs.js
+++ src/typeDefs.js
@@ -1,49 +1,7 @@
 const typeDefs = `#graphql
-  input GroupInput {
-    groupBy: String
-  }
-
-  input AggregateInput {
-    field: String!
-  }
-
-  type Employee {
-    id: ID!
-    name: String!
-    gender: String!
-    age: Int!
-    hireDate: String!
-    terminationDate: String
-    department: String!
-    division: String!
-    jobTitle: String!
-    location: String!
-    employmentStatus: String!
-	}
-
-  type User {
-    id: ID!
-    name: String!
-  }
-
-  type Turnover {
-    date: String!
-    type: String!
-  }
-
-  union Iterable = Employee | User | Turnover | GroupedList
-
-  type GroupedList {
-    group: String
-    list(input: GroupInput): [Iterable!]!
-    total: Int!
-    avg(input: AggregateInput!): Float!
-    sum(input: AggregateInput!): Float!
-  }
-
   type EmployeesMembers {
     count: Int!
-
+    
     id: ID!
     name: String!
     gender: String!
@@ -62,8 +20,6 @@ const typeDefs = `#graphql
   }
 
   type Query {
-    employees(input: GroupInput): [GroupedList!]!
-    turnover(input: GroupInput): [GroupedList!]!
     cube: [Result!]!
   }
 `;
